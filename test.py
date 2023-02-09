02062bed92c12c9d1370a29c9c8df5649a3e0bfc
Jason Zhang <112999853+jasonkzhang@users.noreply.github.com>
2023-02-10 09:43:03 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
-import streamlit as st
+import os
 import psutil
-
+import streamlit as st
 
 def check_chrome():
     for process in psutil.process_iter():
         try:
             if process.name() == "chrome.exe":
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False
+
+def check_tuflow():
+    for process in psutil.process_iter():
+        try:
+            if process.name() == "TUFLOW_iSP_w64.exe":
                 return True
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
@@ -32,6 +41,28 @@ def print_system_info():
     else:
         st.write("Google Chrome is not running.")
 
-st.title("System Information")
-if st.button("Check System Info"):
+    # Check if Tu-Flow is running
+    is_tuflow_running = check_tuflow()
+    if is_tuflow_running:
+        st.write("Tu-Flow is running.")
+    else:
+        st.write("Tu-Flow is not running.")
+    
+    # Get information about users who are logged in
+    result = os.popen("query user").read().strip()
+    lines = result.split("\n")
+    lines = lines[1:]
+    users = []
+    for line in lines:
+        parts = line.split()
+        username = parts[0]
+        session = parts[2]
+        id = parts[3]
+        users.append((username, session, id))
+    st.write("List of logged in users:")
+    for user in users:
+        st.write("- " + user[0] + " (Session " + user[1] + ", ID " + user[2] + ")")
+
+st.title("Cruncher Manager Dashboard")
+if st.button("Click me!"):
     print_system_info()
