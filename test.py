be2dd288a1d67c16af280f0be664f9fc98aa62ba
Jason Zhang <112999853+jasonkzhang@users.noreply.github.com>
2023-02-10 13:32:49 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import streamlit as st
 import getpass
+import socket
 
-st.write("Current User:", getpass.getuser())
+ip = st.text_input("Enter IP address:")
+
+try:
+    hostname = socket.gethostbyaddr(ip)[0]
+    st.write("Hostname:", hostname)
+    st.write("Current User:", getpass.getuser())
+except socket.herror as e:
+    st.write("Could not resolve IP address.")
+
