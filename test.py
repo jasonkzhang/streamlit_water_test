f191fe655a38c1a8c550fae781cdd565aceb672a
Jason Zhang <112999853+jasonkzhang@users.noreply.github.com>
2023-02-10 13:43:36 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 import streamlit as st
-import socket
+import paramiko
+import getpass
 
 ip = st.text_input("Enter IP address:")
+username = st.text_input("Enter username:")
+password = st.text_input("Enter password:", type="password")
 
 try:
-    hostname = socket.gethostbyaddr(ip)[0]
-    st.write("Hostname:", hostname)
-    pc_name = socket.gethostbyname(hostname)
-    st.write("PC Name:", pc_name)
-except socket.herror as e:
-    st.write("Could not resolve IP address.")
-
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, username=username, password=password)
+    stdin, stdout, stderr = ssh.exec_command("whoami")
+    current_user = stdout.read().decode().strip()
+    st.write("Current User:", current_user)
+    ssh.close()
+except Exception as e:
+    st.write("Could not establish connection:", e)
